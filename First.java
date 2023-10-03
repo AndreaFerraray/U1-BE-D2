3e55290b4fb965487a8a01004bc0f85256d71da6
AndreaFerrara13 <138196137+AndreaFerraray@users.noreply.github.com>
2023-10-03 14:26:49 +0200

    first exercise

---
+++ First.java
@@ -0,0 +1,41 @@
+public class First {
+
+    public static void main(String[] args) {
+        String inputString = "allora"; // Puoi cambiare questa stringa con quella di tua scelta
+        boolean risultato = stringaPariDispari(inputString);
+
+        if (risultato) {
+            System.out.println("La stringa ha un numero pari di caratteri.");
+        } else {
+            System.out.println("La stringa ha un numero dispari di caratteri.");
+        }
+    }
+    public static boolean stringaPariDispari(String input) {
+
+        int lunghezza= input.length();
+        if(lunghezza %2==0){
+            return true;
+
+        }
+        else {
+            return false;
+        }
+
+
+
+int  x = 2024;
+        if( x % 4==0 );
+      {
+           return true;
+        }else {
+            return false;
+        }
+
+
+
+
+
+
+
+    }
+}
